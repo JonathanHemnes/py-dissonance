f6af458005b479dbcbb8b7b4a1e78eb78cc20880
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-06-17 10:06:38 -0600

    storage cleanup

--- storage.py
+++ storage.py
@@ -3,13 +3,13 @@ from constants import Constants
 class Storage:
 
     def __init__(self):
-       self.create_tweet_table()
-       self.create_source_name_rows()
+        self.create_tweet_table()
+        self.create_source_name_rows()
 
     def store_latest_tweet_id(self, sourceName, tweet_id):
         conn = self.get_connection()
-        c = conn.cursor()
-        c.execute('UPDATE Tweet SET LastTweet = ? WHERE SourceName = ?', (sourceName, tweet_id))
+        cursor = conn.cursor()
+        cursor.execute('UPDATE Tweet SET LastTweet = ? WHERE SourceName = ?', (sourceName, tweet_id))
         conn.commit()
         conn.close()
 
@@ -18,18 +18,15 @@ class Storage:
 
     def create_tweet_table(self):
         conn = self.get_connection()
-        c = conn.cursor()
-        try:
-            c.execute('CREATE TABLE IF NOT EXISTS Tweet (SourceName text NOT NULL UNIQUE, LastTweet integer)')
-        except Exception as err:
-            print(str(err))
+        cursor = conn.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS Tweet (SourceName text NOT NULL UNIQUE, LastTweet integer)')
         conn.commit()
         conn.close()
     
     def create_source_name_rows(self):
         conn = self.get_connection()
-        c = conn.cursor()
+        cursor = conn.cursor()
         for sourceName in Constants.feeds_to_watch:
-            c.execute('INSERT INTO Tweet(SourceName) SELECT ? WHERE NOT EXISTS(SELECT 1 FROM Tweet WHERE SourceName = ?);', (sourceName, sourceName))
+            cursor.execute('INSERT INTO Tweet(SourceName) SELECT ? WHERE NOT EXISTS(SELECT 1 FROM Tweet WHERE SourceName = ?);', (sourceName, sourceName))
         conn.commit()
         conn.close()
