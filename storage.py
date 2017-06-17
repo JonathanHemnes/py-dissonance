98ba88dffae63fb4ffc649913736201b07e7f265
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-06-17 10:02:44 -0600

    storage working

--- storage.py
+++ storage.py
@@ -3,9 +3,33 @@ from constants import Constants
 class Storage:
 
     def __init__(self):
-        self.conn = sqlite3.connect('tweets.db')
-        # need to initialize table if it doesnt exist
-        #need to initialize sources if they dont exist
+       self.create_tweet_table()
+       self.create_source_name_rows()
 
-    def store_latest_tweet_id(self, source, tweet_id):
-        c = self.conn.cursor()
+    def store_latest_tweet_id(self, sourceName, tweet_id):
+        conn = self.get_connection()
+        c = conn.cursor()
+        c.execute('UPDATE Tweet SET LastTweet = ? WHERE SourceName = ?', (sourceName, tweet_id))
+        conn.commit()
+        conn.close()
+
+    def get_connection(self):
+        return sqlite3.connect('tweets.db')
+
+    def create_tweet_table(self):
+        conn = self.get_connection()
+        c = conn.cursor()
+        try:
+            c.execute('CREATE TABLE IF NOT EXISTS Tweet (SourceName text NOT NULL UNIQUE, LastTweet integer)')
+        except Exception as err:
+            print(str(err))
+        conn.commit()
+        conn.close()
+    
+    def create_source_name_rows(self):
+        conn = self.get_connection()
+        c = conn.cursor()
+        for sourceName in Constants.feeds_to_watch:
+            c.execute('INSERT INTO Tweet(SourceName) SELECT ? WHERE NOT EXISTS(SELECT 1 FROM Tweet WHERE SourceName = ?);', (sourceName, sourceName))
+        conn.commit()
+        conn.close()
--- tweet.py
+++ tweet.py
@@ -2,16 +2,21 @@ import tweepy
 from constants import Constants 
 from finder import Finder
 from replacer import Replacer
+from storage import Storage
 auth = tweepy.OAuthHandler(Constants.consumer_key, Constants.consumer_secret)
 auth.set_access_token(Constants.access_token, Constants.access_token_secret)
 
 api = tweepy.API(auth)
 finder = Finder()
 replacer = Replacer()
+storage = Storage()
 
 for source in Constants.feeds_to_watch:
     print(source)
+
     tweets = api.user_timeline(source)
+    latest_read_tweet_id = tweets[0].id
+    storage.store_latest_tweet_id(source, latest_read_tweet_id)
     trump_related_tweets = finder.get_trump_related_tweets(tweets)
     replaced_tweets = replacer.replace_trump_references(trump_related_tweets)
     cleaned_tweets = replacer.clean_tweets(replaced_tweets)
